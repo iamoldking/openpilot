eaaa53a0a914cd8a992befa16f0a9396193cf76a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-11 17:53:50 -0700

    fixup! test safety on panda

--- openpilot/selfdrive/test/test_safety_panda.sh
+++ openpilot/selfdrive/test/test_safety_panda.sh
@@ -4,7 +4,8 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
 scons -C panda -j"$(nproc)"
-python -c 'from panda import Panda; Panda().flash()'
+python -c 'from panda import Panda; p = Panda(); p.flash(reconnect=False); p.close()'
+sleep 2
 
 export LIBSAFETY_BACKEND=openpilot.selfdrive.test.libsafety_panda
 python -m unittest discover -s opendbc_repo/opendbc/safety/tests -p 'test_*.py'

ba27598f41b849d5fe9d15943e1524c07df6bbfe
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-11 17:58:18 -0700

    fixup! test safety on panda

--- openpilot/selfdrive/test/test_safety_panda.sh
+++ openpilot/selfdrive/test/test_safety_panda.sh
@@ -4,7 +4,19 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 
 scons -C panda -j"$(nproc)"
-python -c 'from panda import Panda; p = Panda(); p.flash(reconnect=False); p.close()'
+python - <<'PY'
+import time
+from panda import Panda
+
+p = Panda()
+p.reset(enter_bootstub=True)
+p.close()
+time.sleep(1)
+p = Panda()
+assert p.bootstub
+p.flash(reconnect=False)
+p.close()
+PY
 sleep 2
 
 export LIBSAFETY_BACKEND=openpilot.selfdrive.test.libsafety_panda
